4ae777f63237f74c32a581f1a3bee3cfbea09dba
samedit66 <samedit66@yandex.ru>
2023-06-24 05:56:40 +0300

    Убрал юникод-артефакты

--- KNPO-Molchanov-PrIn-266/KNPO-Molchanov-PrIn-266.cpp
+++ KNPO-Molchanov-PrIn-266/KNPO-Molchanov-PrIn-266.cpp
@@ -1,24 +1,13 @@
-п»ї#include <iostream>
+#include <iostream>
 #include <cstdio>
 #include <string>
 #include <vector>
 #include <regex>
+#include <memory>
 
 #include "Tokenizer.h"
 
-
 int main()
 {
-	Tokenizer tokenizer;
-
-	std::vector<Token> tokens;
-	tokenizer.tokenize("", tokens);
-
-	std::cout << tokens.size() << std::endl;
-
-	for (const Token& token : tokens) {
-		std::cout << token.text << std::endl;
-	}
-
 	return 0;
 }
